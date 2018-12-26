81a751196cb3c958f5f591137780de5679532db3
randy.hsu <randy.h@netwcom.com>
2018-12-26 14:30:58 +0800

    [Modify] Adjust the flow of creating, join and leave room integrated with MongoDB

--- SocketIoServer.js
+++ SocketIoServer.js
@@ -1,5 +1,14 @@
+// [TODO:] Need to create unique room id (V)
+// [TODO:] Current only one room, Need a DB (V)
+
+// Message Type: {-1: Not a Normal message type, 0: Event Message, 1: Text Message, 2: Image Message, 3: Video Message:, 4: File Message}
+// Event Response Type: {-1: Not a Normal event type, 0: Connected, 1:Create Room, 2: Join Room, 3: Leave Room, 4: Invite member}
+// Room Type: {-1: Not a normal room, 0: single chat room, 1:multiple chat room}
+
 const fs = require('fs');
 const app = require('express')();
+const utils = require('./utils_packs/utils');
+const dbmgr = require('./db_packs/dbmgr');
 const options = {
     key: fs.readFileSync('d:\\NodeJsWorkSpace\\SocketIo\\file.pem'),
     cert: fs.readFileSync('d:\\NodeJsWorkSpace\\SocketIo\\file.crt')
@@ -7,89 +16,105 @@ const options = {
 const server = require('https').createServer(options, app);
 const io = require('socket.io')(server);
 
-// [TODO:] Current only one room, Need a DB
-var roomUserMap = new Map();
+// [TODO:] Time millionseconds need to sync with NTP server
 
-// [TODO:] Need to create unique room id
-var roomId = '1'
 
 // Use websocket only
 // io.set('transports', ['websocket']);
 
-// Message Type: {-1: Not a Normal message type, 0: Event Message, 1: Text Message, 2: Image Message, 3: Video Message:, 4: File Message}
-// Event Response Type: {-1: Not a Normal event type, 0: Connected, 1:Create Room, 2: Join Room, 3: Leave Room}
-// Room Type: {-1: Not a normal room, 0: single chat room, 1:multiple chat room}
+// Connect to MongoDB
+dbmgr.connect((is_connected) => {
+    if (!is_connected) {
+        console.log('[LOG:] DB connect fail...');
+    } else {
+        console.log('[LOG:] DB connect success...');
 
-io.on('connection', (socket) => {
-    var token = socket.handshake.query.auth_token;
-    console.log(token + " connected");
-    console.log("Auth_Token = " + token);
+        io.on('connection', (socket) => {
+            let token = socket.handshake.query.auth_token;
+            var room_id = null;
 
-    socket.on('disconnect', () => {
-        socket.emit('leave-room', roomId);
-        console.log(socket.handshake.query.auth_token + ' disconnected');
-    });
+            console.log("[LOG:] " + token + " connected");
+            console.log("[LOG:] Auth_Token = " + token);
 
-    socket.on('create-room', () => {
-        // [TODO:] Check exist room for same members
-        // [TODO:] Time millionseconds need to sync with NTP server
-        var currentTimeMillions = new Date().getTime();
+            socket.on('disconnect', () => {
+                console.log('[LOG:] ' + token + ' disconnected from ' + room_id);
+            });
 
-        socket.emit('create-room-success', roomId);
-    })
+            socket.on('create-room', (room_type, user_info_list) => {
+                // [TODO:] Check exist room for same members
+                dbmgr.createNewRoomId((new_room_id) => {
+                    if (!new_room_id) {
+                        return;
+                    }
 
-    socket.on('join-room', (roomId, userInfoJsonStr) => {
-        socket.join(roomId, () => {
-            let rooms = Object.values(socket.rooms);
-            let currentTimeMillions = new Date().getTime;
-            let userInfo = JSON.parse(userInfoJsonStr);
+                    room_id = new_room_id;
+                    var room_info = {
+                        "room_id": room_id,
+                        "room_type": room_type,
+                        "unread_count": 0,
+                        "user_info_list": JSON.parse(user_info_list),
+                        "last_message_timestamp": -1,
+                        "last_message": ""
+                    };
 
-            if (!roomUserMap.has(userInfo.user_id)) {
-                roomUserMap.set(userInfo.user_id, userInfo);
-            }
-            let roomUserInfoJsonAryStr = JSON.stringify(Array.from(roomUserMap.values()));
+                    //[TODO:] Need to sync room member list
+                    dbmgr.updateRoomInfo(room_info, (is_update_roominfo_success) => {
+                        if (!is_update_roominfo_success) {
+                            return;
+                        }
+                    
+                        socket.emit('create-room-success', room_info);
+                    });
+                });
+            });
 
-            var eventMsg = {
-                "room_id": roomId,
-                "message_type": 0,
-                "event_response_type": 2,
-                "message_time": currentTimeMillions,
-                "message": roomUserInfoJsonAryStr
-            }
+            socket.on('join-room', (room_id, userInfoJsonStr) => {
+                socket.join(room_id, () => {
+                    let rooms = Object.values(socket.rooms);
+                    let currentTimeMillions = new Date().getTime;
+                    let userInfo = JSON.parse(userInfoJsonStr);
 
-            socket.emit('join-room-success', roomId);
-            io.sockets.in(roomId).emit('receive-message', eventMsg);
+                    var eventMsg = {
+                        "room_id": room_id,
+                        "message_type": 0,
+                        "event_response_type": 2,
+                        "message_time": currentTimeMillions,
+                        "message": userInfoJsonStr
+                    }
+
+                    socket.emit('join-room-success', room_id);
+                    io.sockets.in(room_id).emit('receive-message', eventMsg);
+                });
+            })
+
+            socket.on('leave-room', (room_id, userInfoJsonStr) => {
+                console.log('[LOG:] ' + token + ' leave room ' + room_id);
+
+                let currentTimeMillions = new Date().getTime;
+                let userInfo = JSON.parse(userInfoJsonStr);
+
+                var eventMsg = {
+                    "room_id": room_id,
+                    "message_type": 0,
+                    "event_response_type": 3,
+                    "message_time": currentTimeMillions,
+                    "message": userInfoJsonStr
+                }
+
+                io.sockets.in(room_id).emit('receive-message', eventMsg);
+                socket.leave(room_id);
+            })
+
+            socket.on('send-message', (msgInfoJsonStr) => {
+                var msgInfoObj = JSON.parse(msgInfoJsonStr);
+                let room_id = msgInfoObj.room_id;
+
+                io.sockets.in(room_id).emit('receive-message', msgInfoJsonStr);
+            })
         });
-    })
-
-    socket.on('leave-room', (roomId, userInfoJsonStr) => {
-        let currentTimeMillions = new Date().getTime;
-        let userInfo = JSON.parse(userInfoJsonStr);
-
-        if (roomUserMap.has(userInfo.user_id)) {
-            roomUserMap.delete(userInfo.user_id);
-        }
-        let roomUserInfoJsonAryStr = JSON.stringify(Array.from(roomUserMap.values()));
-
-        var eventMsg = {
-            "room_id": roomId,
-            "message_type": 0,
-            "event_response_type": 3,
-            "message_time": currentTimeMillions,
-            "message": roomUserInfoJsonAryStr
-        }
-
-        io.sockets.in(roomId).emit('receive-message', eventMsg);
-        socket.leave(roomId);
-    })
-
-    socket.on('send-message', (msgInfoJsonStr) => {
-        var msgInfoObj = JSON.parse(msgInfoJsonStr);
-        var roomId = msgInfoObj.room_id;
-
-        io.sockets.in(roomId).emit('receive-message', msgInfoJsonStr);
-    })
+        server.listen(3000, () => {
+            console.log("[LOG:] Server listening...")
+        });
+    }
 });
-server.listen(3000, () => {
-    console.log("Server listening...")
-});
+
