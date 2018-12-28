412e366dee7c6808c69f315c0846f2a656a91eda
randy.hsu <randy.h@netwcom.com>
2018-12-28 16:57:41 +0800

    [Adjust] Add api for sign_up, sign_in and login_in

--- SocketIoChatApi/routes/users.js
+++ SocketIoChatApi/routes/users.js
@@ -18,41 +18,56 @@ router.post('/sign_up', function (req, res) {
   let user_pwd = req.body.user_pwd;
   var user_info = { 'user_name': user_name, 'user_pwd': user_pwd };
 
-  console.log('[LOG: sign_up] user_name = ' + user_name + ' user_pwd = ' + user_pwd)
+  console.log('[LOG: sign_up] >>> /sign_up');
+  console.log('[LOG: sign_up] /sign_up, user_name = ' + user_name + ' user_pwd = ' + user_pwd);
 
-  dbmgr.query_user_info(user_info, (result) => {
+  dbmgr.query_user_info(user_info, (is_success, result) => {
     console.log("[LOG: sign_up] query_user_info, result = " + result);
 
-    if (result) {
-      res.status(200).json({
-        'status': 200,
-        'payload': {
-          'user_id': result.user_id,
-          'user_name': result.user_name,
-          'user_pwd': result.user_pwd
-        }
-      });
-      res.end();
+    if (is_success) {
+      if (result) {
+        res.status(200).json({
+          'status': 200,
+          'payload': {
+            'user_id': result.user_id,
+            'user_name': result.user_name,
+            'user_pwd': result.user_pwd
+          }
+        });
+  
+        console.log('[LOG: sign_up] <<< /sign_up');
+  
+        res.end();
+      } else {
+        dbmgr.create_user_info(user_info, (is_success, result) => {
+          if (is_success) {
+  
+            console.log("[LOG: query_user_info] create new user, result = " + result);
+  
+            res.status(200).json({
+              'status': 200,
+              'payload': {
+                'user_id': result.user_id,
+                'user_name': result.user_name,
+                'user_pwd': result.user_pwd
+              }
+            });
+          } else {
+            res.status(404).json({
+              'status': 404,
+              'message': 'request error'
+            });
+          }
+          console.log('[LOG: sign_up] <<< /sign_up');
+  
+          res.end();
+        });
+      }      
     } else {
-      dbmgr.create_user_info(user_info, (result) => {
-        if (result) {
-          console.log("[LOG: query_user_info] create new user, result = " + result);
-          res.status(200).json({
-            'status': 200,
-            'payload': {
-              'user_id': result.user_id,
-              'user_name': result.user_name,
-              'user_pwd': result.user_pwd
-            }
-          });
-        } else {
-          res.status(404).json({
-            'status': 404,
-            'message': 'request error'
-          });
-        }
-        res.end();        
-      });
+      res.status(404).json({
+        'status': 404,
+        'message': 'request error'
+      });      
     }
   });
 });
@@ -61,17 +76,27 @@ router.post('/sign_in', function (req, res) {
   let user_name = req.body.user_name;
   let user_pwd = req.body.user_pwd;
 
-  dbmgr.query_user_info({ 'user_name': user_name, 'user_pwd': user_pwd }, (result) => {
-    console.log("[LOG: sign_in] query_user_info, result = " + result);
+  console.log('[LOG: sign_in] >>> /sign_in');
+  console.log('[LOG: sign_in] /sign_in, user_name = ' + user_name + ' user_pwd = ' + user_pwd);
 
-    if (result) {
-      res.status(200).json({
-        'status': 200,
-        'payload': {
+  dbmgr.query_user_info({ 'user_name': user_name, 'user_pwd': user_pwd }, (is_success, result) => {
+
+    if (is_success) {
+      let payload = {};
+
+      console.log("[LOG: sign_in] query_user_info, result = " + result);
+
+      if (result) {
+        payload = {
           'user_id': result.user_id,
           'user_name': result.user_name,
           'user_pwd': result.user_pwd
-        }
+        };
+      }
+
+      res.status(200).json({
+        'status': 200,
+        'payload': payload
       });
     } else {
       res.status(400).json({
@@ -79,12 +104,36 @@ router.post('/sign_in', function (req, res) {
         'message': 'request error'
       });
     }
+
+    console.log('[LOG: sign_in] <<< /sign_in');
+
     res.end();
   });
 });
 
-router.get('/fetch_user_list', function (req, res) {
-  res.send('respond with fetch_user_data');
+router.get('/user_info_list', function (req, res) {
+  console.log('[LOG: user_info_list] >>> /user_info_list');
+
+  dbmgr.query_user_info_list((is_success, result) => {
+    if (is_success) {
+      res.status(200).json({
+        'status': 200,
+        'payload': result
+      });
+
+      console.log('[LOG: sign_in] /user_info_list, user_info_list = ' + result);
+
+    } else {
+      res.status(400).json({
+        'status': 400,
+        'message': 'request error'
+      });
+    }
+
+    console.log('[LOG: user_info_list] <<< /user_info_list');
+
+  });
+
 });
 
 module.exports = router;
