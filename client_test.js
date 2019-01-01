b7cb6eb492658d03411e11dc4ed9f981119afff5
Randy <o1984531@gmail.com>
2019-01-01 22:12:46 +0800

    [Modify] Adjust the testing client js file naming

--- client_test.js
+++ client_test.js
@@ -1,21 +1,38 @@
 const Mongo = require('mongodb');
 const Assert = require('assert');
+const Redis = require('redis');
 
 const MongoClient = Mongo.MongoClient;
 const server = 'localhost:27017';
+const db_name = 'Test'
 const user = encodeURIComponent('user1');
 const pwd = encodeURIComponent('user1');
 const authMechanism = 'DEFAULT';
 const url = `mongodb://${user}:${pwd}@${server}/?authMechanism=${authMechanism}&authSource=admin`;
-const client = new MongoClient(url);
+const mongo_client = new MongoClient(url);
 
-client.connect((err) => {
+// const redis_server_url= 'redis://localhost:6379';
+// const redis_client = Redis.createClient(redis_server_url);
+
+
+// redis_client.on('connect', () => {
+//     console.log('Redis db connected...');
+// });
+// redis_client.set('foo', 'bar', Redis.print);
+// redis_client.get('foo', (err, result) => {
+//     Assert.equal(err, null);
+
+//     console.log('Get result = ' + result);
+// });
+
+mongo_client.connect((err, mongo_client) => {
     Assert.equal(err, null)
 
     console.log("Mongo db connected...");
 
-    const db = client.db('TestDB');
-    db.collection('TestTable2').createIndex({'column6':1}, {'w':1});
+    const db = mongo_client.db(db_name);
+    
+    db.collection('TestTable').createIndex({ 'column6': 1 }, { 'w': 1 });
 
     db.collection('TestTable').find().toArray((err, datas) => {
         var i = 0;
@@ -25,7 +42,7 @@ client.connect((err) => {
         });
     });
 
-    db.collection('TestTable2').insertMany([{ "column4": "val6", "column5": "val7" }, { "column6": "val8", "column7": "val9", "column8": "val10" }], (err, result) => {
+    db.collection('TestTable').insertMany([{ "column4": "val6", "column5": "val7" }, { "column6": "val8", "column7": "val9", "column8": "val10" }], (err, result) => {
         Assert.equal(err, null);
 
         for (doc of result.ops) {
@@ -35,7 +52,7 @@ client.connect((err) => {
     })
 
     let targetId = new Mongo.ObjectID('5c1de7c68e56c80b306b5c94');
-    db.collection('TestTable2').find({ '_id': targetId}).toArray((err, docs) => {
+    db.collection('TestTable').find({ '_id': targetId }).toArray((err, docs) => {
         Assert.equal(err, null);
 
         for (doc of docs) {
@@ -44,17 +61,17 @@ client.connect((err) => {
     })
 
     targetId = new Mongo.ObjectID('5c1ded10d3d752563458b11e');
-    db.collection('TestTable2').update({"_id": targetId}, {$set:{"column8": "updated_val10"}}, (err, result) => {
+    db.collection('TestTable').update({ "_id": targetId }, { $set: { "column8": "updated_val10" } }, (err, result) => {
         Assert.equal(err, null);
 
         console.log("Changed data count is " + result.result.n);
     })
 
-    db.collection('TestTable2').deleteOne({"_id": targetId}).then(response => {
+    db.collection('TestTable').deleteOne({ "_id": targetId }).then(response => {
         console.log(response);
     }, reject => {
         console.log(reject);
     })
 
-    
+
 });
